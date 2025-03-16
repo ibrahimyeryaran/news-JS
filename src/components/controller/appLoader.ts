4b5d73699b35015469ab5c24575ba6193e207cd5
ibrahim yeryaran <ibrahimyeryaran93@gmail.com>
2025-03-16 10:31:00 +0300

    fix

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,8 +3,7 @@ import { Endpoints, LoaderOptions } from '../../types';
 
 export default class AppLoader extends Loader {
     constructor() {
-
-        super('https://newsapi.org/v2/', { apiKey: 'b99cd6c90b45475781384b6b58c50962' });
+        super('https://newsapi.org/v2/', { apiKey: '9b3631ad3932443fbb4be22d973320e5' });
     }
 
     public getResp<T>(params: { endpoint: Endpoints; options?: LoaderOptions }, callback: (data: T) => void): void {
